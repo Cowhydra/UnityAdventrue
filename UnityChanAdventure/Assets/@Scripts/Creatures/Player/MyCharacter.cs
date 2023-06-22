695a2b8161b4255da224cbd11b5f8d3963165b6a
y00nhye <126942402+Cowhydra@users.noreply.github.com>
2023-06-22 17:58:24 +0900

    [fix]2
    [fix]2

--- UnityChanAdventure/Assets/@Scripts/Creatures/Player/MyCharacter.cs
+++ UnityChanAdventure/Assets/@Scripts/Creatures/Player/MyCharacter.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-public class MyCharacter:Creature,IDamage,IListener
+public class MyCharacter : Creature, IDamage, IListener
 {
     private int myCharacterCode;
 
@@ -11,28 +11,32 @@ public class MyCharacter:Creature,IDamage,IListener
     public int MaxHp { get { return _maxhp; } private set { _maxhp = value; } }
     public int MaxMana { get { return _maxmana; } private set { _maxmana = value; } }
     public int MagicDef { get { return _magicdef; } private set { _magicdef = value; } }
-    public int Def { get { return _def; }  private set { _def = value; } }
-    public int MagicAttack { get { return _magicattack; } private set  { _magicattack=value; } }
-    public int Attack { get { return _attack; } private set  { _attack = value; } }
+    public int Def { get { return _def; } private set { _def = value; } }
+    public int MagicAttack { get { return _magicattack; } private set { _magicattack = value; } }
+    public int Attack { get { return _attack; } private set { _attack = value; } }
     public int AttackSpeed { get { return _attackspeed; } private set { _attackspeed = value; } }
     public string Name { get { return Managers.Data.CharacterDataDict[myCharacterCode].name; } private set { } }
-    public int Level 
+    public int Level
     {
-        get { return _level; } private set 
+        get { return _level; }
+        private set
         {
             _level = value;
             Managers.Data.CharacterDataDict[Managers.Game.currentCharNumber].level = _level;
             Debug.Log("캐릭터 레벨업 하면 DB 갱신 ");
             InitCharacter();
-        } 
+        }
     }
     public Define.MonsterAttackType EnemyAttackType = Define.MonsterAttackType.Melee;
     private int _mana;
-    public int Mana { get { return _mana; } 
-        set 
+    public int Mana
+    {
+        get { return _mana; }
+        set
         {
             _mana = Math.Clamp(value, 0, _maxmana);
-        } }
+        }
+    }
     private int _exp;
     public int Exp
     {
@@ -47,7 +51,7 @@ public class MyCharacter:Creature,IDamage,IListener
 
                 Debug.Log("캐릭터 경험치를 획득하게 되면 DB를 갱신해야 합니다.");
             }
-            
+
         }
     }
     public int Hp
@@ -74,7 +78,7 @@ public class MyCharacter:Creature,IDamage,IListener
         isDie = false;
     }
 
-    private void  InitCharacter()
+    private void InitCharacter()
     {
         if (!Managers.Data.CharacterDataDict.ContainsKey(myCharacterCode))
         {
@@ -85,15 +89,15 @@ public class MyCharacter:Creature,IDamage,IListener
         }
 
         _maxhp = Managers.Data.CharacterDataDict[myCharacterCode].maxhp + Managers.EQUIP.EQUIP_MaxHp;
-        _maxmana= Managers.Data.CharacterDataDict[myCharacterCode].maxmana + Managers.EQUIP.EQUIP_MaxMp;
+        _maxmana = Managers.Data.CharacterDataDict[myCharacterCode].maxmana + Managers.EQUIP.EQUIP_MaxMp;
         _hp = MaxHp;
         _mana = Mana;
         _def = Managers.Data.CharacterDataDict[myCharacterCode].def + Managers.EQUIP.EQUIP_Def + (Level - 1) * 10;
-        _magicdef= Managers.Data.CharacterDataDict[myCharacterCode].magicdef + Managers.EQUIP.EQUIP_MagicDef + (Level - 1) * 10;
-        _magicattack= Managers.Data.CharacterDataDict[myCharacterCode].magicattack + Managers.EQUIP.EQUIP_MagicAttack + (Level - 1) * 10;
+        _magicdef = Managers.Data.CharacterDataDict[myCharacterCode].magicdef + Managers.EQUIP.EQUIP_MagicDef + (Level - 1) * 10;
+        _magicattack = Managers.Data.CharacterDataDict[myCharacterCode].magicattack + Managers.EQUIP.EQUIP_MagicAttack + (Level - 1) * 10;
         _attack = Managers.Data.CharacterDataDict[myCharacterCode].attack + Managers.EQUIP.EQUIP_Attack + (Level - 1) * 10;
         _attackspeed = Managers.Data.CharacterDataDict[myCharacterCode].attackspeed;
-        _level= Managers.Data.CharacterDataDict[myCharacterCode].level;
+        _level = Managers.Data.CharacterDataDict[myCharacterCode].level;
         Managers.Event.PostNotification(Define.EVENT_TYPE.PlayerStatsChange, this);
     }
     private void CharacterEQUIPStatsChange()
@@ -107,7 +111,7 @@ public class MyCharacter:Creature,IDamage,IListener
         Managers.Event.PostNotification(Define.EVENT_TYPE.PlayerStatsChange, this);
     }
     #endregion
-    public void OnDamage(int damage,Define.MonsterAttackType MonsterAttackType)
+    public void OnDamage(int damage, Define.MonsterAttackType MonsterAttackType)
     {
         if (MonsterAttackType == Define.MonsterAttackType.RangeAttack)
         {
@@ -115,9 +119,9 @@ public class MyCharacter:Creature,IDamage,IListener
         }
         else
         {
-            Hp -= Math.Max(0,damage - _level - _def);
+            Hp -= Math.Max(0, damage - _level - _def);
         }
-      
+
     }
     public override void Die()
     {
@@ -135,7 +139,7 @@ public class MyCharacter:Creature,IDamage,IListener
         while (true)
         {
             Mana += 5;
-            Hp +=5;
+            Hp += 5;
             yield return new WaitForSeconds(_healthRegenDelay);
         }
     }
@@ -150,5 +154,5 @@ public class MyCharacter:Creature,IDamage,IListener
         }
     }
 
- 
+
 }
