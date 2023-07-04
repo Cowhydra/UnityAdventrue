e82b61d2724ce366b76a5f6f980e5aa977489695
KangYunSeok <cowhydra@naver.com>
2023-07-04 22:09:29 +0900

    [fix] camera
     일단 OK .. 내일 할일 - (물 던전 보스 패턴 확인  스킬 준비?

--- UnityChanAdventure/Assets/@Scripts/Contents/Camera/CameraController.cs
+++ UnityChanAdventure/Assets/@Scripts/Contents/Camera/CameraController.cs
@@ -5,23 +5,38 @@ using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
-    CinemachineVirtualCamera camera;
-    GameObject player;
-    private Vector3 _direction;
+    public CinemachineVirtualCamera camera;
+    public GameObject player;
+
+    private CinemachineFramingTransposer framingTransposer;
 
     private void Start()
     {
         camera = GetComponent<CinemachineVirtualCamera>();
+        framingTransposer = camera.GetCinemachineComponent<CinemachineFramingTransposer>();
         player = GameObject.FindGameObjectWithTag("Player");
-        _direction = (gameObject.transform.position - player.transform.position).normalized;
-
     }
+
     private void Update()
     {
-        //RaycastHit raycastHit;
-        //if(Physics.Raycast(gameObject.transform.position,_direction*10,1<< (int)Define.LayerMask.Enviroment,out raycastHit))
-        //{
+        RaycastHit hit;
+        Vector3 cameraPosition = transform.position;
+        Vector3 playerPosition = player.transform.position;
+        Vector3 direction = (playerPosition - cameraPosition).normalized;
 
-        //}
+        if (Physics.Raycast(cameraPosition, direction, out hit, Mathf.Infinity, 1<<(int)Define.LayerMask.Enviroment))
+        {
+            float distanceToWall = hit.distance;
+            float targetDistance = Mathf.Clamp(distanceToWall - 0.5f, 3, 10);
+
+            // 카메라 거리 조정
+            framingTransposer.m_CameraDistance = Mathf.Lerp(framingTransposer.m_CameraDistance, targetDistance, Time.deltaTime);
+        }
+        else
+        {
+            // 원래 카메라 거리로 복원
+            float originalDistance = 10f; // 원하는 초기 카메라 거리 설정
+            framingTransposer.m_CameraDistance = Mathf.Lerp(framingTransposer.m_CameraDistance, originalDistance, Time.deltaTime);
+        }
     }
 }
